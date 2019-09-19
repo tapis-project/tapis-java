0173bc90b2179c8d4ffafba8372194c6e602fd7c
Joe Meiring <jmeiring@tacc.utexas.edu>
2019-09-19 13:44:54 -0500

    session cache

--- tapis-fileslib/src/main/java/edu/utexas/tacc/tapis/files/lib/cache/ClientSessionCache.java
+++ tapis-fileslib/src/main/java/edu/utexas/tacc/tapis/files/lib/cache/ClientSessionCache.java
@@ -9,19 +9,23 @@ import java.util.concurrent.TimeUnit;
 
 public class ClientSessionCache {
 
-  private static Cache<String, String> sessionCache;
-  private CacheLoader loader;
-  private RemovalListener listener;
-
-  public ClientSessionCache () {
-
-    loader = new ClientSessionCacheLoader();
-    listener = new ClientSessionCacheRemovalListener();
-    sessionCache =  CacheBuilder.newBuilder()
-        .expireAfterWrite(1, TimeUnit.MINUTES)
+  private static CacheLoader loader = new ClientSessionCacheLoader();
+  private static RemovalListener listener = new ClientSessionCacheRemovalListener();
+  private static Cache<String, String> sessionCache =  CacheBuilder.newBuilder()
+      .expireAfterWrite(1, TimeUnit.MINUTES)
         .removalListener(listener)
         .build();
+  private static final ClientSessionCache instance = new ClientSessionCache();
+
+  private ClientSessionCache () {}
+
+
+  public static Cache getInstance() {
+    return sessionCache;
   }
 
+  public static String get(String key){
+    return sessionCache.getIfPresent(key);
+  }
 
 }
