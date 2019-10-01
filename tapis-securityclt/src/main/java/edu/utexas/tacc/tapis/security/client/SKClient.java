122cc78d7b6b2de4ce13402825c7785bf87b5cfb
Richard Cardone <rcardone@tacc.utexas.edu>
2019-10-01 13:10:22 -0500

    securityclt initialized

---
+++ tapis-securityclt/src/main/java/edu/utexas/tacc/tapis/security/client/SKClient.java
@@ -0,0 +1,29 @@
+package edu.utexas.tacc.tapis.security.client;
+
+import java.net.URL;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SKClient 
+{
+    /* **************************************************************************** */
+    /*                                   Constants                                  */
+    /* **************************************************************************** */
+    // Local logger.
+    private static final Logger _log = LoggerFactory.getLogger(SKClient.class);
+    
+    /* **************************************************************************** */
+    /*                                Public Methods                                */
+    /* **************************************************************************** */
+    /* ---------------------------------------------------------------------------- */
+    /* createRole:                                                                  */
+    /* ---------------------------------------------------------------------------- */
+    URL createRole(String roleName, String description)
+    {
+        URL url = null;
+        
+        return url;
+    }
+    
+}
