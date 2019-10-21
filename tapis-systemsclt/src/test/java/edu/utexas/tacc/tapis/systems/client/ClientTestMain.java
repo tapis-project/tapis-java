012f4b6739fa7b7c210a925e6daa67994c10d343
Richard Cardone <rcardone@tacc.utexas.edu>
2019-10-21 13:23:56 -0500

    fix system compile

--- tapis-systemsclt/src/test/java/edu/utexas/tacc/tapis/systems/client/ClientTestMain.java
+++ tapis-systemsclt/src/test/java/edu/utexas/tacc/tapis/systems/client/ClientTestMain.java
@@ -1,19 +1,19 @@
 package edu.utexas.tacc.tapis.systems.client;
 
-import edu.utexas.tacc.tapis.systems_client.ApiClient;
-import edu.utexas.tacc.tapis.systems_client.ApiResponse;
-import edu.utexas.tacc.tapis.systems_client.api.SystemsApi;
+//import edu.utexas.tacc.tapis.systems_client.ApiClient;
+//import edu.utexas.tacc.tapis.systems_client.ApiResponse;
+//import edu.utexas.tacc.tapis.systems_client.api.SystemsApi;
 
 public class ClientTestMain
 {
   public static void main(String[] args) throws Exception
   {
-    System.out.println("Starting Systems ClientTest");
-    ApiClient  apiClient = new ApiClient();
-    apiClient.setBasePath("http://localhost:8080/systems/v3");
-    apiClient.setDebugging(true);
-    SystemsApi sysApi = new SystemsApi(apiClient);
-    ApiResponse resp = sysApi.getSystemByNameWithHttpInfo("system1", Boolean.TRUE, Boolean.FALSE);
-    System.out.println(resp);
+//    System.out.println("Starting Systems ClientTest");
+//    ApiClient  apiClient = new ApiClient();
+//    apiClient.setBasePath("http://localhost:8080/systems/v3");
+//    apiClient.setDebugging(true);
+//    SystemsApi sysApi = new SystemsApi(apiClient);
+//    ApiResponse resp = sysApi.getSystemByNameWithHttpInfo("system1", Boolean.TRUE, Boolean.FALSE);
+//    System.out.println(resp);
   }
 }
