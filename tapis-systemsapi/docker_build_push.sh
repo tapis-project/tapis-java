aea6d02915904e8fe47e79c702e748092c1932da
Steve Black <scblack@tacc.utexas.edu>
2019-11-11 17:54:05 -0600

    Update docker_build_push script for running in Jenkins

--- tapis-systemsapi/docker_build_push.sh
+++ tapis-systemsapi/docker_build_push.sh
@@ -2,7 +2,9 @@
 export VER=`cat target/systems/WEB-INF/classes/tapis.version`
 export GIT_COMMIT=`awk '{print $2}' target/systems/WEB-INF/classes/git.info`
 export TAG="tapis/systems:${VER}"
-export TAG2="elj4321/ptapsystems:${VER}"
+export TAG2="tapis/systems:${VER}"
+# Login to docker. Credentials set by Jenkins
+docker login -u $USERNAME -p $PASSWD
 # Build image from Dockerfile
 docker build --build-arg VER=${VER} --build-arg GIT_COMMIT=${GIT_COMMIT} -t ${TAG} .
 # Create tagged image for remote repo
