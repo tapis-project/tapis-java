7d6279e30c60a9983d65f30916ab492a81b35526
Steve Black <51328106+scblack321@users.noreply.github.com>
2019-11-12 14:15:31 -0600

    Updates for using tapis-bom, tapis-shared-java from local maven repo (#1)
    * Updates for using tapis-bom, tapis-shared-java from local maven repo
    
    * Revert BOM version back to 1.2-SNAPSHOT
    
    * Remove shared code now in tapis-shared-java repo

--- tapis-sharedapi/src/main/java/edu/utexas/tacc/tapis/sharedapi/responses/results/ResultAuthorized.java
+++
@@ -1,6 +0,0 @@
-package edu.utexas.tacc.tapis.sharedapi.responses.results;
-
-public final class ResultAuthorized
-{
-    public boolean isAuthorized;
-}
--- tapis-shareddb/src/main/java/edu/utexas/tacc/tapis/shareddb/datasource/DataSourceProvider.java
+++
@@ -1,8 +0,0 @@
-package edu.utexas.tacc.tapis.shareddb.datasource;
-
-import javax.sql.DataSource;
-
-public interface DataSourceProvider 
-{
-    DataSource getDataSource();
-}
