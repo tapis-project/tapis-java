154454fc2f41640c332bd03bbe9e23b97c3dab65
Steve Black <scblack@tacc.utexas.edu>
2020-03-05 17:21:56 -0600

    Update systemsapi docker build scripts for standalone shaded fat jar

--- tapis-systemsapi/docker_run.sh
+++ tapis-systemsapi/docker_run.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-export VER=`cat target/v3#systems/WEB-INF/classes/tapis.version`
+# export VER=`cat target/v3#systems/WEB-INF/classes/tapis.version`
+# export GIT_COMMIT=`awk '{print $2}' target/v3#systems/WEB-INF/classes/git.info`
+export VER="0.0.1"
+export GIT_COMMIT="abcdefg"
+
 export TAG="tapis/systems:${VER}"
 # docker run -d --rm --network="host" -p 7070:8080 -p 7000:8000 ${TAG}
 # Running with network=host exposes ports directly. Only works for linux
