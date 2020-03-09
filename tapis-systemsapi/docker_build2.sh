5a720a8066690e0759720ae623c4b0e93e98a4b3
Steve Black <scblack@tacc.utexas.edu>
2020-03-09 15:51:53 -0500

    update docker build script

--- tapis-systemsapi/docker_build2.sh
+++ tapis-systemsapi/docker_build2.sh
@@ -3,16 +3,16 @@
 # export GIT_COMMIT=`awk '{print $2}' target/v3#systems/WEB-INF/classes/git.info`
 # export TAG="tapis/systems:${VER}"
 # export TAG2="tapis/systems:${VER}"
+export TAG3="tapis/systems:0.0.1"
 # Login to docker. Credentials set by Jenkins
-# docker login -u $USERNAME -p $PASSWD
+docker login -u $USERNAME -p $PASSWD
 # Build image from Dockerfile
 # docker build --build-arg VER=${VER} --build-arg GIT_COMMIT=${GIT_COMMIT} -t ${TAG} .
-export TAG3="tapis/systems-grizz"
 set -xv
 docker build -f Dockerfile2 -t ${TAG3} .
 # Create tagged image for remote repo
 # docker tag $TAG $TAG2
 # Push to remote repo
-# if [ "x$1" = "x-push" ]; then
-#   docker push $TAG2
-# fi
+if [ "x$1" = "x-push" ]; then
+  docker push $TAG3
+fi
