8e21c236c20d94a14811cafd5d87d8bc7bf170fd
Steve Black <scblack@tacc.utexas.edu>
2020-03-09 16:25:18 -0500

    Update docker local run script

--- tapis-systemsapi/docker_run2.sh
+++ tapis-systemsapi/docker_run2.sh
@@ -3,6 +3,6 @@
 # export TAG="tapis/systems:${VER}"
 # docker run -d --rm --network="host" -p 7070:8080 -p 7000:8000 ${TAG}
 # Running with network=host exposes ports directly. Only works for linux
-export TAG3="tapis/systems-grizz"
+export TAG3="tapis/systems:0.0.1"
 set -xv
 docker run -d --rm --network="host" ${TAG3}
