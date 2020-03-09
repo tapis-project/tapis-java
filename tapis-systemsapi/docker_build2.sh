a7cc7ae6f0a2b7b7a8ed8d38457bf761f0f4c61f
Steve Black <scblack@tacc.utexas.edu>
2020-03-09 15:41:30 -0500

    Fix issue with docker build

--- tapis-systemsapi/docker_build2.sh
+++ tapis-systemsapi/docker_build2.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-set -xv
-export VER=`cat target/v3#systems/WEB-INF/classes/tapis.version`
-export GIT_COMMIT=`awk '{print $2}' target/v3#systems/WEB-INF/classes/git.info`
-export TAG="tapis/systems:${VER}"
-export TAG2="tapis/systems:${VER}"
-export TAG3="tapis/systems-grizz"
+# export VER=`cat target/v3#systems/WEB-INF/classes/tapis.version`
+# export GIT_COMMIT=`awk '{print $2}' target/v3#systems/WEB-INF/classes/git.info`
+# export TAG="tapis/systems:${VER}"
+# export TAG2="tapis/systems:${VER}"
 # Login to docker. Credentials set by Jenkins
 # docker login -u $USERNAME -p $PASSWD
 # Build image from Dockerfile
 # docker build --build-arg VER=${VER} --build-arg GIT_COMMIT=${GIT_COMMIT} -t ${TAG} .
+export TAG3="tapis/systems-grizz"
+set -xv
 docker build -f Dockerfile2 -t ${TAG3} .
 # Create tagged image for remote repo
 # docker tag $TAG $TAG2
--- tapis-systemsapi/docker_run2.sh
+++ tapis-systemsapi/docker_run2.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-export VER=`cat target/v3#systems/WEB-INF/classes/tapis.version`
-export TAG="tapis/systems:${VER}"
-export TAG3="tapis/systems-grizz"
+# export VER=`cat target/v3#systems/WEB-INF/classes/tapis.version`
+# export TAG="tapis/systems:${VER}"
 # docker run -d --rm --network="host" -p 7070:8080 -p 7000:8000 ${TAG}
 # Running with network=host exposes ports directly. Only works for linux
-set -v
+export TAG3="tapis/systems-grizz"
+set -xv
 docker run -d --rm --network="host" ${TAG3}
