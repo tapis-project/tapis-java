fc5053a8c4f26f48674cbfad6016d8f20bf12760
Steve Black <scblack@tacc.utexas.edu>
2020-06-20 12:41:28 -0500

    Systems: Merge in docker_build updates from tapis-systems-dev branch

--- deployment/tapis-systemsapi/docker_build.sh
+++ deployment/tapis-systemsapi/docker_build.sh
@@ -1,8 +1,42 @@
 #!/bin/sh
+# Build and optionally push docker image for Systems service
+# This is the job run in Jenkins as part of job Systems-<ENV>-Build-Test-Publish-Images
+# Environment name must be passed in as first argument
+# Existing docker login is used for push
+# Docker image is created with a unique tag: tapis/systems-<ENV>-<VER>-<COMMIT>-<YYYYmmddHHMM>
+#   - other tags are created and updated as appropriate
+#
+# REPO env var may be set to push to $REPO/systems. Default is tapis/systems
+
 PrgName=$(basename "$0")
 
-# Build docker image for Systems service
+USAGE="Usage: $PrgName { dev staging prod } [ -push ]"
+
+if [ -z "$REPO" ]; then
+  REPO=tapis
+fi
+
 BUILD_DIR=../../tapis-systemsapi/target
+ENV=$1
+PUSH=$2
+
+# Check number of arguments
+if [ $# -lt 1 -o $# -gt 2 ]; then
+  echo $USAGE
+  exit 1
+fi
+
+# Check that env name is valid
+if [ "$ENV" != "dev" -a "$ENV" != "staging" -a "$ENV" != "prod" ]; then
+  echo $USAGE
+  exit 1
+fi
+
+# Check second arg
+if [ $# -eq 2 -a "x$2" != "x-push" ]; then
+  echo $USAGE
+  exit 1
+fi
 
 # Determine absolute path to location from which we are running.
 export RUN_DIR=$(pwd)
@@ -23,24 +57,39 @@ cp Dockerfile $BUILD_DIR
 cd $BUILD_DIR || exit
 
 # Set variables used for build
-export VER=$(cat classes/tapis.version)
-export GIT_BRANCH=$(awk '{print $1}' classes/git.info)
-export GIT_COMMIT=$(awk '{print $2}' classes/git.info)
-export TAG="tapis/systems:${VER}"
-export TAG2="tapis/systems:${VER}"
+VER=$(cat classes/tapis.version)
+GIT_BRANCH=$(awk '{print $1}' classes/git.info)
+GIT_COMMIT=$(awk '{print $2}' classes/git.info)
+TAG1="${REPO}/systems:${ENV}-${VER}-${GIT_COMMIT}-$(date +%Y%m%d%H%M)"
+TAG2="${REPO}/systems:${ENV}"
+TAG3="${REPO}/systems:latest"
 
 # Build image from Dockerfile
-docker build -f Dockerfile --build-arg VER="${VER}" \
-    --build-arg GIT_COMMIT="${GIT_COMMIT}" --build-arg GIT_BRANCH="${GIT_BRANCH}" \
-    -t "${TAG}" .
+echo "Building local image using primary tag: $TAG1"
+echo "  ENV=        ${ENV}"
+echo "  VER=        ${VER}"
+echo "  GIT_BRANCH= ${GIT_BRANCH}"
+echo "  GIT_COMMIT= ${GIT_BRANCH}"
+docker build -f Dockerfile \
+   --label VER="${VER}" --label GIT_COMMIT="${GIT_COMMIT}" --label GIT_BRANCH="${GIT_BRANCH}" \
+    -t "${TAG1}" .
 
-# Create tagged image for remote repo
-docker tag "$TAG" "$TAG2"
+# Create second tag for remote repo
+echo "Creating second image tag: $TAG2"
+docker tag "$TAG1" "$TAG2"
 
 # Push to remote repo
-if [ "x$1" = "x-push" ]; then
-  # Login to docker. Credentials set by Jenkins
-  docker login -u "$USERNAME" -p "$PASSWD"
+if [ "x$2" = "x-push" ]; then
+  if [ "$ENV" = "prod" ]; then
+    echo "Creating third image tag for prod env: $TAG3"
+    docker tag "$TAG1" "$TAG3"
+  fi
+  echo "Pushing images to docker hub."
+  # NOTE: Use current login. Jenkins job does login
+  docker push "$TAG1"
   docker push "$TAG2"
+  if [ "$ENV" = "prod" ]; then
+    docker push "$TAG3"
+  fi
 fi
 cd "$RUN_DIR"
