4a0dc3984c190891b4f59211b3e3cfc8b1da7dde
Richard Cardone <rcardone@tacc.utexas.edu>
2021-03-05 17:14:29 -0600

    test program tweaks

--- tapis-jobsapi/src/test/java/edu/utexas/tacc/tapis/jobs/api/driver/ListFiles.java
+++ tapis-jobsapi/src/test/java/edu/utexas/tacc/tapis/jobs/api/driver/ListFiles.java
@@ -8,7 +8,7 @@ import edu.utexas.tacc.tapis.files.client.FilesClient;
 public class ListFiles 
 {
     // ***************** Configuration Parameters *****************
-    private static final String BASE_URL   = "https://dev.develop.tapis.io";
+    private static final String BASE_URL = "https://dev.develop.tapis.io";
     private static final String userJWT = 
         "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJqdGkiOiIxOTVhNzU2YS1hZWRhLTQ2NjQtODFhMS1hODg1ZDZhZmJiNDMiLCJpc3MiOiJodHRwczovL2Rldi5kZXZlbG9wLnRhcGlzLmlvL3YzL3Rva2VucyIsInN1YiI6InRlc3R1c2VyMkBkZXYiLCJ0YXBpcy90ZW5hbnRfaWQiOiJkZXYiLCJ0YXBpcy90b2tlbl90eXBlIjoiYWNjZXNzIiwidGFwaXMvZGVsZWdhdGlvbiI6ZmFsc2UsInRhcGlzL2RlbGVnYXRpb25fc3ViIjpudWxsLCJ0YXBpcy91c2VybmFtZSI6InRlc3R1c2VyMiIsInRhcGlzL2FjY291bnRfdHlwZSI6InVzZXIiLCJleHAiOjE5MjU4MzgyMzF9.wbyeWa6PQpROtnPWpykKc9ln2TQj04cD_uwjS40UeF5PMDJ7jd5u8GJ0JPyaH-qj9R3H9-J4H9vQGPnKQg7Wqj9_QIja9t5g5WM7Vz70TaXmu91EO3_rbJkmguXZMRFdBS0YFDYGLccO2i50NVyt3i-nVRAp3nFCn5-eB6UEoU_KEe5MiFnMmuzF6kUIGDi6Cw_26DxI_SsY-zcpjCmX0jx5cM0xqLv8XNv1RIVr8o9fKGuvGupdT0ZdTCp_MiMBPi11OE7OCYo7iwp-yglcpOMlQF8LOCJe9txzJGqcCZSGbQBi4mNLasyYn0cstVak9ToGQpEpiSdz4FvQtSKT9A";
     
@@ -18,7 +18,6 @@ public class ListFiles
     
     /** Driver
      * 
-     * @param args should contain the name of a file in the ./requests directory
      * @throws Exception on error
      */
     public static void main(String[] args) throws Exception
