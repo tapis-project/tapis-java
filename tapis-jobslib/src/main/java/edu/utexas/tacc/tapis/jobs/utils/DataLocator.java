90d0d88af541dfceab49ed7df5d147e3f63a94e1
Smruti Padhy <spadhy@tacc.utexas.edu>
2022-09-22 13:17:40 -0400

    Improved code readability

--- tapis-jobslib/src/main/java/edu/utexas/tacc/tapis/jobs/utils/DataLocator.java
+++ tapis-jobslib/src/main/java/edu/utexas/tacc/tapis/jobs/utils/DataLocator.java
@@ -52,8 +52,7 @@ public class DataLocator {
     	 String systemId = "";
     	 String systemUrl = "";
     	 JobOutputInfo jobOutputInfo = null;
-    	 boolean isArchiveSystem = true;
-    	
+    	    	
     	 // archiveSytemDir and archiveSystemId always have values set
     	 if(_job.getStatus() == JobStatusType.FINISHED || 
     	    (_job.getStatus()== JobStatusType.FAILED && 
@@ -63,11 +62,13 @@ public class DataLocator {
     		systemUrl = makeSystemUrl( _job.getArchiveSystemDir(), pathName);
     		_log.debug("Archive Path URL: " + systemUrl);
     		
-    		jobOutputInfo =  new JobOutputInfo(systemId,systemId, systemUrl,isArchiveSystem);
+    		final boolean isArchiveSystem = true;
+    		jobOutputInfo =  new JobOutputInfo(systemId, systemId, systemUrl, isArchiveSystem);
     	 } else {
     		 systemId = _job.getExecSystemId();
     		 systemUrl = makeSystemUrl( _job.getExecSystemOutputDir(), pathName);
-     		jobOutputInfo =  new JobOutputInfo(systemId,systemId, systemUrl,!isArchiveSystem);
+    		 final boolean isArchiveSystem = false;
+     		jobOutputInfo =  new JobOutputInfo(systemId, systemId, systemUrl, isArchiveSystem);
     	 }
     	 
     	 return jobOutputInfo ;
